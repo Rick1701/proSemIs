ae2ff72f1cb5d2e1c7d1510dc4816e2b26f0d697
Bennix <ben.martinezj@gmail.com>
2023-05-15 10:06:08 -0400

    base,est_uni,uaerea,uterrestre(controllers,models,routes,services)

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -13,6 +13,17 @@ const authMiddleware = require("../middlewares/authe.middleware.js");
 // Importa el enrutador de siniestro
 const siniestroRoutes = require("./siniestro.routes.js");
 
+
+const baseRoutes = require("./base.routes.js");
+
+const estado_unidadRoutes = require("./estado_unidad.routes.js");
+
+const uaereaRoutes = require("./uaerea.routes.js");
+
+const uterrestreRoutes = require("./uterrestre.routes.js");
+
+
+
 // Crea una instancia del enrutador
 const router = express.Router();
 
@@ -22,6 +33,14 @@ router.use("/users", authMiddleware.verifyToken, userRoutes);
 router.use("/auth", authRoutes);
 // Define las rutas para los siniestros /api/siniestro
 router.use("/siniestro", siniestroRoutes);
+
+router.use("/base",baseRoutes);
+router.use("/estado_unidad", estado_unidadRoutes);
+router.use("/uaerea",uaereaRoutes);
+router.use("/uterrestre",uterrestreRoutes);
+
+
+
 
 // Exporta el enrutador
 module.exports = router;
