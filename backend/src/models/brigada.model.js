f146aabdc44b6e3a59ec6779a883b00267af0360
Ricardo Flores Fuentes <ricardo.flores1701@alumnos.ubiobio.cl>
2023-05-16 22:22:19 -0400

    resolviendo faltantes de brigada y brigadista (mod, cont, rout, serv)

---
+++ backend/src/models/brigada.model.js
@@ -0,0 +1,27 @@
+"use strict";
+const { number } = require("joi");
+// Importa el modulo 'mongoose' para crear la conexion a la base de datos
+const mongoose = require("mongoose");
+
+// Crea el esquema de la coleccion 'brigada'
+const brigadaSchema = new mongoose.Schema({
+  bri_nombre: {
+    type: String,
+    required: true,
+  },
+  bri_cantidad: {
+    type: Number,
+    required: true,
+  },
+  bri_especialidad: {
+    type: String,
+    required: true,
+  }
+ ,
+});
+
+// Crea el modelo de datos 'Brigada' a partir del esquema 'brigadaSchema'
+const Brigada = mongoose.model("Brigada", brigadaSchema);
+
+// Exporta el modelo de datos 'Brigada'
+module.exports = Brigada;
